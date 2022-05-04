0a16991af08d77911f5b2328178d2bdec37a97a7
Jonathan Aldas <jonaaldas@gmail.com>
2022-05-04 17:09:01 -0500

    example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  // created state to save the sting form the input
+  const [error, setError] = useState({
+    error: ''
+  })
+
+
+  function handleChange(evt) {
+    setError({ error: evt.target.value });
+    console.log(error)
+  }
+  function submit(event) {
+    event.preventDefault();
+    if (error.error) {
+      console.log('We have something')
+    } else {
+      console.log('please write something')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label>
+          First name
+          <input
+            type="text"
+            value={error.error}
+            onChange={handleChange}
+          />
+        </label>
+        <button onClick={submit}>Submit</button>
+      </form>
     </div>
   );
 }
